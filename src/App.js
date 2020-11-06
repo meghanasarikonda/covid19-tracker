b2b2f905f311d2d7b7c8bf3175c3fd630bbde466
meghanasarikonda <meghana94987@gmail.com>
2020-11-06 16:26:21 +0530

    update readme

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ function App() {
   return (
     <div className="app">
       <h1>Hello World. Let's build covid19-tracker</h1>
-      <Header></Header>
     </div>
   );
-}^
+}
 
 export default App;
-6
