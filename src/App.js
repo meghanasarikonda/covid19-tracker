662317701f9fe89b2e6a113ffbd124dea6c8e656
meghanasarikonda <meghana94987@gmail.com>
2020-11-06 16:23:33 +0530

    first comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello World. Let's build covid19-tracker</h1>
+      <Header></Header>
+    </div>
+  );
+}^
+
+export default App;
+6
